0569ae38cc86215f82deb53692a66d272532c605
JohnnyGoldSoft <114264011+johnnygoldsoft@users.noreply.github.com>
2026-03-22 20:18:38 +0000

    Fix: Remove HTTPS scheme enforcement in production
    Remove URL force scheme for production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +21,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if (app()->environment('production')) {
-        URL::forceScheme('https');
-    }
     }
 }
